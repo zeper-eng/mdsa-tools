009effeecf5734bb3fd4eef701377225ff192605
Luis Perez <luis@Luiss-MacBook-Pro.local>
2025-08-06 13:01:20 -0400

    edititng setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='TBD_v_v',                        # Name of the package
-    version='0.1.0',                         # Version number
+    name='TBD_v_v',                    
+    version='0.1.0',                         
     description="The Weir Labs H-bond Systems Analyses modules!",
     author='Luis Perez',                     
     author_email='lperez@wesleyan.edu',   
-    packages=find_packages(),                # Automatically finds all packages with __init__.py
-    include_package_data=True,               # If you have data files inside packages
+    packages=find_packages(),                
+    include_package_data=True,               
     install_requires=[
         'numpy',
         'pandas',
@@ -21,5 +21,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',                 # Or whatever version you’re targeting
+    python_requires='>=3.7',                
 )
