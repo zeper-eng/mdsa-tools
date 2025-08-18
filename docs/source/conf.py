1e150278bf442ecb8985dff7ed1b6335498eb95b
Luis Perez <luis@Luiss-MacBook-Pro.local>
2025-08-17 21:50:03 -0400

    fixing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,53 +1,32 @@
-extensions += ["nbsphinx"]
-nbsphinx_execute = "never"   # don’t run heavy code on RTD
+# -- Project info -------------------------------------------------------------
 
-# docs/source/conf.py
-import os, sys
-sys.path.insert(0, os.path.abspath("../.."))  # adjust if needed
+project = "mdsa-tools"
+author = "Weir Lab"
+release = "0.1"
+
+# -- General config -----------------------------------------------------------
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
-]
-
-# Avoid building native/scientific deps on RTD
-autodoc_mock_imports = [
-    "numpy", "pandas", "matplotlib", "sklearn",
-    "umap", "umap_learn", "mdtraj",
-    "python_circos", "pycircos", "requests",
-]
-
-html_theme = "sphinx_rtd_theme"
-autosummary_generate = True
-
-html_title = "mdsa-tools Documentation"
-html_short_title = "mdsa-tools"
-
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "myst_parser",     # if you have Markdown in docs
+    "nbsphinx",        # if you render notebooks
 ]
 
 autosummary_generate = True
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
 
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "show-inheritance": True,
-    # optional extras:
-    # "inherited-members": True,
-    # "private-members": True,   # include _private
-    # "special-members": "__call__",  # dunder methods if you want
-}
+# If heavy libs break RTD/Actions, mock them here:
+autodoc_mock_imports = ["mdtraj", "umap", "python_circos", "matplotlib", "seaborn"]
 
-# If some heavy deps break import at doc build, mock them:
-autodoc_mock_imports = ["mdtraj"]  # add others if needed
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 
-# Ensure the package can be found
-import os, sys
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../../'))
+# -- Options for HTML output --------------------------------------------------
+
+html_theme = "alabaster"   # or "sphinx_rtd_theme" if you install it
+html_static_path = ["_static"]
 
