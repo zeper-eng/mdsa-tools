f7a8188e16fd6f25cf2887043d28dfd90736db9d
Luis Perez <luis@Luiss-MacBook-Pro.local>
2025-09-14 11:53:56 -0400

    small cpptraj import test

--- tests/test_cpptraj_import.py
+++ tests/test_cpptraj_import.py
@@ -3,5 +3,5 @@ import numpy as np
 def test_importer_file_creation(importer):
     rep=importer.create_systems_rep()
     print(rep)
-    np.save(f'./tests/test_output/GCU_1_in_10_s',)
+    np.save(f'./tests/test_output/GCU_1_in_10_s',rep)
     return
