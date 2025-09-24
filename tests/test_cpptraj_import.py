c43ee52a180ac9f136da039348891cb1a5c2f56b
Luis Perez <luis@Luiss-MacBook-Pro.local>
2025-09-24 14:09:18 -0400

    trying to get ci back in the greeen with all theese references

--- tests/test_cpptraj_import.py
+++ tests/test_cpptraj_import.py
@@ -3,7 +3,6 @@ import numpy as np
 def test_importer_file_creation(importer):
     rep=importer.create_systems_rep()
     print(rep)
-    np.save(f'./tests/test_output/GCU_1_in_10_s',rep)
     return
 
 # tests/test_cpptraj_import_extra.py
